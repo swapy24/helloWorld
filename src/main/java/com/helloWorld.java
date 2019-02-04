908bcb3da44fb8fb3da73525d08ec1955413f1c2
swapy24 <swapnilaher24@yahoo.com>
2019-02-04 15:39:00 +0530

    this is my code

---
+++ src/main/java/com/helloWorld.java
@@ -0,0 +1,8 @@
+package com;
+
+public class helloWorld {
+
+    public void printName(){
+
+    }
+}
---
+++ src/test/java/com/helloWorldTest.java
@@ -0,0 +1,18 @@
+package com;
+
+import static org.junit.Assert.*;
+
+public class helloWorldTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void printName() {
+    }
+}
